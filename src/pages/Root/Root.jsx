f022c892c7bf3a77421bf0ff767be36f8acb2c22
SadiaSultana26 <eee12016026.brur@gmail.com>
2025-11-26 17:58:22 +0600

    Footer

---
+++ src/pages/Root/Root.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Outlet } from "react-router";
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/navbar";
+
+const Root = ()  => {
+    return (
+        <div>
+          <Navbar></Navbar>
+           <Outlet></Outlet>
+           <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root ;
